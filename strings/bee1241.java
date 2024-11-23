74deca7963cef04fdce19d116c028c22b11cb170
Paulo Lopes <pocostalopes@gmail.com>
2024-11-23 03:31:39 -0300

    nova pasta

---
+++ strings/bee1241.java
@@ -0,0 +1,34 @@
+package strings;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class bee1241 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+
+        n = sc.nextInt();
+
+        while (n > 0) {
+
+            String a, b;
+
+            a = sc.next();
+            b = sc.next();
+
+            if (a.endsWith(b)) {
+                System.out.println("encaixa");
+            } else {
+                System.out.println("nao encaixa");
+            }
+
+            n--;
+        }
+
+        sc.close();
+    }
+}
